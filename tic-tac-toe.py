edad52b6e1b91f6fc96a8b8f45280bf0d2e1eca0
TeePlunder <leon.bergmann23@gmail.com>
2023-03-22 13:46:52 +0100

    first setup

---
+++ tic-tac-toe.py
@@ -0,0 +1,36 @@
+import os
+from time import sleep
+# print("HAllo")
+# print("HAllo")
+# print("HAllo")
+# print("HAllo")
+# print("HAllo")
+# print("HAllo")
+# print("HAllo")
+
+# print("sleep")
+# sleep(2)
+
+# clear console
+
+
+def clear_console(): return os.system("clear")
+
+
+# clear_console()
+
+# print("1")
+# print("1")
+# print("1")
+# print("1")
+
+# t = input("WElches feld? =")
+
+# print(t)
+
+# text = ""
+# for x in range(100):
+#     print(text)
+#     text += "| "
+#     sleep(0.8)
+#     clear_console()
