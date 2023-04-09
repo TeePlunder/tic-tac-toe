4642ee5d692fb775bbc1d5fbda2507aa8c978b83
TeePlunder <leon.bergmann23@gmail.com>
2023-04-09 17:37:15 -0300

    place a symbol and find available positions

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -18,14 +18,23 @@ def calc_mid(number):
     return round(number / 2)
 
 
-# playground config
+# PLAYGROUND CONFIG #
 COLUMN_COUNT = 3
 SPACE_TO_SYMBOL = 2
 SELECTION_FIELD_WIDTH = 5
 PLAYGROUND_WIDTH = SELECTION_FIELD_WIDTH * COLUMN_COUNT + SPACE_TO_SYMBOL
 
+# GAME SETUPS #
+# these are the 9 posible positons to choose from
+# 0 - open position
+# 1 - player 1 field
+# 2 - player 2 field
+# only positions with status 0 can be edited
+positions_status = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+avabilable_positions = []
 
-def draw_playground():
+
+def create_playground():
     playground = ""
     for row in range(COLUMN_COUNT):
         line = ""
@@ -46,4 +55,21 @@ def draw_playground():
     return playground
 
 
-print(draw_playground())
+def set_available_positions():
+    for index, position in enumerate(positions_status):
+        if position == 0:
+            avabilable_positions.append(index)
+
+
+def place_symbol(symbol, position):
+    position_index = position - 1
+    if position_index in avabilable_positions:
+        positions_status[position_index] = symbol
+
+
+print(create_playground())
+set_available_positions()
+print(place_symbol(1, 1))
+print(place_symbol(2, 5))
+print(positions_status)
+# p = input("PLAYER X [Symbol: Z]: Which Position would you take?\t")
