82745d4aa0f3bbdc88222992af89b0e3a2c9def4
TeePlunder <leon.bergmann23@gmail.com>
2023-04-09 17:06:32 -0300

    draw the hole playground

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -18,10 +18,6 @@ def calc_mid(number):
     return round(number / 2)
 
 
-def add_new_line(string):
-    return string + "\n"
-
-
 # playground config
 COLUMN_COUNT = 3
 SPACE_TO_SYMBOL = 2
@@ -29,21 +25,23 @@ SELECTION_FIELD_WIDTH = 5
 PLAYGROUND_WIDTH = SELECTION_FIELD_WIDTH * COLUMN_COUNT + SPACE_TO_SYMBOL
 
 
-def draw_playground_line():
-    line = ""
-    for field in range(COLUMN_COUNT):
-        for selection_field in range(SELECTION_FIELD_WIDTH):
-            if selection_field == calc_mid(SELECTION_FIELD_WIDTH):
-                line = line + "-"
-                continue
-            line = line + " "
-        if field != COLUMN_COUNT - 1:
-            line = line + "|"
-    line = add_new_line(line)
-    for x in range(PLAYGROUND_WIDTH):
-        line = line + "-"
-    line = add_new_line(line)
-    print(line)
+def draw_playground():
+    for row in range(COLUMN_COUNT):
+        line = ""
+        for field in range(COLUMN_COUNT):
+            for selection_field in range(SELECTION_FIELD_WIDTH):
+                if selection_field == calc_mid(SELECTION_FIELD_WIDTH):
+                    line = line + "-"
+                    continue
+                line = line + " "
+            if field != COLUMN_COUNT - 1:
+                line = line + "|"
+        print(line)
+        middle_line = ""
+        if row != COLUMN_COUNT - 1:
+            for x in range(PLAYGROUND_WIDTH):
+                middle_line = middle_line + "-"
+            print(middle_line)
 
 
-draw_playground_line()
+draw_playground()
