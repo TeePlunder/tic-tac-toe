13d905d6c5acbc0c81cf3b2cdf0a8dfdb3846de1
TeePlunder <leon.bergmann23@gmail.com>
2023-04-09 15:12:53 -0400

    add vertical line

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -18,22 +18,31 @@ def calc_mid(number):
     return round(number / 2)
 
 
+def add_new_line(string):
+    return string + "\n"
+
+
 # playground config
 COLUMN_COUNT = 3
+SPACE_TO_SYMBOL = 2
 SELECTION_FIELD_WIDTH = 5
-PLAYGROUND_WIDTH = SELECTION_FIELD_WIDTH * 3
+PLAYGROUND_WIDTH = SELECTION_FIELD_WIDTH * COLUMN_COUNT + SPACE_TO_SYMBOL
 
 
 def draw_playground_line():
     line = ""
     for field in range(COLUMN_COUNT):
-        for x in range(SELECTION_FIELD_WIDTH):
-            if x == calc_mid(SELECTION_FIELD_WIDTH):
+        for selection_field in range(SELECTION_FIELD_WIDTH):
+            if selection_field == calc_mid(SELECTION_FIELD_WIDTH):
                 line = line + "-"
                 continue
             line = line + " "
         if field != COLUMN_COUNT - 1:
             line = line + "|"
+    line = add_new_line(line)
+    for x in range(PLAYGROUND_WIDTH):
+        line = line + "-"
+    line = add_new_line(line)
     print(line)
 
 
